9a4fdaff26d8893c610679553c5fc607007e5614
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2016-05-23 21:46:00 +0200

    [Step-0] code clean (NOT WORKING !!!) for jspm presentation of MoisDuJS_2016

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-(function () {
-    const slots = {1: [], 2: [], 3: [], 4: []};
-    const grid = document.querySelector('x-slot-grid');
+const init = () => {
+  const slots = {1: [], 2: [], 3: [], 4: []};
+  const grid = document.querySelector('x-slot-grid');
 
-    document
-      .querySelector('x-slot-form')
-      .addEventListener('add-slot', function (event) {
-        slots[event.detail.slot].push(event.detail.name);
-        grid.setAttribute('data', JSON.stringify(slots));
-      }, true);
-})();
+  document
+    .querySelector('x-slot-form')
+    .addEventListener('add-slot', function(event) {
+      slots[event.detail.slot].push(event.detail.name);
+      grid.setAttribute('data', JSON.stringify(slots));
+    }, true);
+};
+
+init();
--- slot-form/x-slot-form.js
+++ slot-form/x-slot-form.js
@@ -1,46 +1,34 @@
-(function () {
+class XSlotForm extends HTMLElement {
+  createdCallback() {
+    this.innerHTML = template;
 
-    const owner = document.currentScript.ownerDocument;
+    this.slotColumn = this.querySelector('#slot-column');
+    this.slotName = this.querySelector('#slot-name');
 
-    class XSlotForm extends HTMLElement {
-        createdCallback() {
-            this.setupComponent();
-        }
+    this.querySelector('#add-slot')
+      .addEventListener('click', this.onClickAddSlot.bind(this), false);
+    this.querySelector('#add-slot')
+      .addEventListener('submit', this.onClickAddSlot.bind(this), false);
+  }
 
-        setupComponent() {
-            const template = owner.querySelector('#x-slot-form');
-            const clone = template.content.cloneNode(true);
-            this.appendChild(clone);
-
-            this.slotColumn = this.querySelector('#slot-column');
-            this.slotName = this.querySelector('#slot-name');
-
-            this.querySelector('#add-slot')
-                .addEventListener('click', this.onClickAddSlot.bind(this), false);
-            this.querySelector('#add-slot')
-                .addEventListener('submit', this.onClickAddSlot.bind(this), false);
-        }
-
-        onClickAddSlot(e) {
-            e.preventDefault();
-            const event = new CustomEvent('add-slot', {
-                detail: {
-                    slot: this.slotColumn.value,
-                    name: this.slotName.value
-                }
-            });
-            if (event.detail.name && event.detail.slot) {
-                this.dispatchEvent(event);
-                this.reset();
-            }
-        }
-
-        reset() {
-            this.slotColumn.value = 1;
-            this.slotName.value = '';
-        }
+  onClickAddSlot(e) {
+    e.preventDefault();
+    const event = new CustomEvent('add-slot', {
+      detail: {
+        slot: this.slotColumn.value,
+        name: this.slotName.value
+      }
+    });
+    if (event.detail.name && event.detail.slot) {
+      this.dispatchEvent(event);
+      this.reset();
     }
+  }
 
-    document.registerElement('x-slot-form', XSlotForm);
+  reset() {
+    this.slotColumn.value = 1;
+    this.slotName.value = '';
+  }
+}
 
-})();
+document.registerElement('x-slot-form', XSlotForm);
