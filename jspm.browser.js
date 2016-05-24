4a9c3ff0f04369d8026b2f16539760a5eaa7fb3c
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2016-05-24 14:14:40 +0200

    [Step-3] JSPM Bundle

--- jspm.browser.js
+++ jspm.browser.js
@@ -4,5 +4,25 @@ SystemJS.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*",
     "moisdujs-jspm/": "src/"
+  },
+  bundles: {
+    "build.js": [
+      "moisdujs-jspm/app.js",
+      "moisdujs-jspm/slot-grid/x-slot-grid.js",
+      "moisdujs-jspm/slot-grid/x-slot-grid.css!github:systemjs/plugin-css@0.1.21/css.js",
+      "github:systemjs/plugin-css@0.1.21.json",
+      "moisdujs-jspm/slot-grid/x-slot-grid.html!github:systemjs/plugin-text@0.0.8/text.js",
+      "github:systemjs/plugin-text@0.0.8.json",
+      "npm:systemjs-plugin-babel@0.0.10/babel-helpers/inherits.js",
+      "npm:systemjs-plugin-babel@0.0.10.json",
+      "npm:systemjs-plugin-babel@0.0.10/babel-helpers/possibleConstructorReturn.js",
+      "npm:systemjs-plugin-babel@0.0.10/babel-helpers/createClass.js",
+      "npm:systemjs-plugin-babel@0.0.10/babel-helpers/classCallCheck.js",
+      "moisdujs-jspm/slot-form/x-slot-form.js",
+      "moisdujs-jspm/slot-form/x-slot-form.css!github:systemjs/plugin-css@0.1.21/css.js",
+      "moisdujs-jspm/slot-form/x-slot-form.html!github:systemjs/plugin-text@0.0.8/text.js",
+      "moisdujs-jspm/style/app.scss!github:mobilexag/plugin-sass@0.4.3/index.js",
+      "github:mobilexag/plugin-sass@0.4.3.json"
+    ]
   }
 });
